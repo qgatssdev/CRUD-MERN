32b8126f0d6663975bac92a568afc417b91c8159
qgatssdev <chivoomodu@gmail.com>
2022-03-31 14:23:09 +0100

    initailizing database with sql

--- client/vite-number-conversion/src/App.tsx
+++ client/vite-number-conversion/src/App.tsx
@@ -1,22 +1,53 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState('');
+  const [position, setPosition] = useState('');
+  const [wage, setWage] = useState(0);
+
   return (
     <div className="App">
       <div className="information">
         <label>Name</label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
         <label>Age</label>
-        <input type="number" />
+        <input
+          type="number"
+          onChange={(event) => setAge(parseInt(event.target.value))}
+        />
         <label>Country</label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={(event) => {
+            setCountry(event.target.value);
+          }}
+        />
         <label>Position</label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={(event) => {
+            setPosition(event.target.value);
+          }}
+        />
         <label>Wage (year)</label>
-        <input type="number" />
+        <input
+          type="number"
+          onChange={(event) => {
+            setWage(parseInt(event.target.value));
+          }}
+        />
         <button>Add Employer</button>
       </div>
     </div>
   );
 }
 export default App;
+ 
